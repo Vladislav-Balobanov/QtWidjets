532c3c76b19e3490a882f41cae96199a731d8c46
Vladislav-Balobanov <vlad.velikan@gmail.com>
2024-03-12 15:33:42 +0700

    Перенос проекта 2.7 с dev на example_2_7

---
+++ QtWidjets2_7/win.h
@@ -0,0 +1,42 @@
+#pragma once
+#ifndef win_h
+#define win_h
+#include <QtGui>
+#include <QFrame>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QMessageBox>
+
+// Основное окно приложения
+class Win : public QWidget
+{
+    Q_OBJECT
+protected:
+    QTextCodec* _codec;			// Текстовый кодек
+    QFrame* _frame;				// Рамка
+    QLabel* _inputLabel;		// Метка редактора ввода
+    QLabel* _outputLabel;		// Метка редактора вывода
+    QLineEdit* _inputEdit;		// Редактор ввода
+    QLineEdit* _outputEdit;		// Редактор вывода
+    QPushButton* _nextButton;	// Кнопка "Следующий"
+    QPushButton* _exitButton;	// Кнопка "Выход"
+public:
+    Win(QWidget* parent = 0);	// Конструктор окна
+public slots:
+    void begin();	// Начальная настройка интерфейса
+    void calc();	// Реализация вычислений
+};
+
+// Валидатор ввода
+class StrValidator : public QValidator
+{
+public:
+    StrValidator(QObject* parent) :QValidator(parent) {}
+    virtual State validate(QString& str, int& position) const
+    {
+        return Acceptable;
+    }
+};
+#endif
